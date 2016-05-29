2a96cf7eb3e419578d69051775655c4e362bee28
crakama <crakama89@gmail.com>
2016-05-29 19:36:30 +0300

    fetch and save user referenceID

--- mainapp/models.py
+++ mainapp/models.py
@@ -5,9 +5,11 @@ from django.db import models
 
 class Main(models.Model):
     email = models.EmailField(unique=True)
+    ref_id = models.CharField(max_length=120, default='ReferenceID')
     ip_address = models.CharField(max_length=120, default='IPAddress')
     timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
     updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+
 
     def __unicode__(self):
         return "{}".format(self.email)
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,4 @@
+import uuid
 from django.shortcuts import render
 from .forms import EmailForm
 from .models import Main
@@ -18,6 +19,16 @@ def get_ip(request):
     return ip
 
 
+def get_ref_id():
+    '''A function to generate referenceID '''
+    ref_id = str(uuid.uuid4())[:11].replace('-', '').lower()
+    try:
+        id_exists = Main.objects.get(ref_id=ref_id)
+        get_ref_id()
+    except:
+        return ref_id
+
+
 def home(request):
     ''' A function that handles the learnmore submit button on homepage'''
     form = EmailForm(request.POST or None)
@@ -26,6 +37,7 @@ def home(request):
         email = form.cleaned_data['email']
         view, created = Main.objects.get_or_create(email=email)
         if created:
+            view.ref_id = get_ref_id()
             view.ip_address = get_ip(request)
             view.save()
     context = {"form": form}
